ab60a23431c2e4604270c2ef7beda6e2416042de
Trent Hekman <122338142+Trent008@users.noreply.github.com>
2024-01-20 18:46:08 -0800

    hypersimplify

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -7,16 +7,52 @@
 #include <frc/TimedRobot.h>
 #include "frc/XboxController.h"
 #include "fmt/core.h"
-#include "Swerve.h"
+#include "rev/CANSparkMax.h"
+#include "ctre/phoenix6/CANcoder.hpp"
+#include "ctre/phoenix6/TalonFX.hpp"
+#include "AHRS.h"
+
+using namespace std;
+
+class SwerveModule
+{
+
+private:
+    ctre::phoenix6::hardware::TalonFX *dMotor;
+    rev::CANSparkMax *sMotor;
+    ctre::phoenix6::hardware::CANcoder *encoder;
+    complex<float> turnVector;
+
+public:
+    SwerveModule(int moduleID, complex<float> turnVector) {
+        dMotor = new ctre::phoenix6::hardware::TalonFX(10+moduleID, "rio");
+        sMotor = new rev::CANSparkMax{30+moduleID, rev::CANSparkMax::MotorType::kBrushless};
+        encoder = new ctre::phoenix6::hardware::CANcoder{20+moduleID};
+        this->turnVector = turnVector;
+    }
+
+    void Set(complex<float> driveRate, float turnRate) {
+        complex<float> moduleVector = driveRate+turnVector*turnRate;
+        float error = arg(moduleVector) - encoder->GetAbsolutePosition().GetValue().value()*M_PI*2;
+        if (error < -M_PI) {
+        error += M_PI*2;
+        }
+        if (error > M_PI) {
+            error -= M_PI*2;
+        }
+        dMotor->Set(abs(moduleVector));
+        sMotor->Set(error / M_PI);
+    }
+};
 
 class Robot : public frc::TimedRobot {
  public:
   SwerveModule modules[4] =
   {
-    SwerveModule{1, {-1, -1}},
-    SwerveModule{2, {1, -1}},
-    SwerveModule{3, {-1, 1}},
-    SwerveModule{4, {1, 1}}};
+    SwerveModule{1, complex<float>(-1, -1)},
+    SwerveModule{2, complex<float>(1, -1)},
+    SwerveModule{3, complex<float>(-1, 1)},
+    SwerveModule{4, complex<float>(1, 1)}};
 
   AHRS navx{frc::SPI::Port::kMXP};
 
@@ -38,7 +74,6 @@ class Robot : public frc::TimedRobot {
   void SimulationInit() override;
   void SimulationPeriodic() override;
 
+ private:
   frc::XboxController xBoxC{0};
-  int i = 0;
-  Swerve swerve;
 };
