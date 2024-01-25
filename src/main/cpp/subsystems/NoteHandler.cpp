ee752255418790ba2946cbc18e44f70e4349c0d1
Trent Hekman <122338142+Trent008@users.noreply.github.com>
2024-01-24 20:52:54 -0800

    finish NoteHandler definition

--- src/main/cpp/subsystems/NoteHandler.cpp
+++ src/main/cpp/subsystems/NoteHandler.cpp
@@ -6,20 +6,60 @@
 
 NoteHandler::NoteHandler() = default;
 
-void NoteHandler::Initialize() {
-    // m_elevator.SetInverted(false);
-    // elevatorPID.SetP(0.2);
-    // elevatorPID.SetI(0);
-    // elevatorPID.SetD(1);
-    // elevatorPID.SetFF(0);
-    // elevatorPID.SetSmartMotionAccelStrategy(rev::SparkMaxPIDController::AccelStrategy::kTrapezoidal);
-    // elevatorPID.SetSmartMotionMaxAccel(2000);
-    // elevatorPID.SetSmartMotionMaxVelocity(1000);
-    // m_angle.SetInverted(false);
-    // m_rollers.SetInverted(false);
+// set the elevator height (0 to 20 inches)
+bool NoteHandler::SetHeight(float height, float maxRPM = 1000, float tolerance = 1) {
+    elevatorPID.SetSmartMotionMaxVelocity(maxRPM);
+    elevatorPID.SetReference(height, rev::ControlType::kSmartMotion);
+    return abs(height - e_elevator.GetPosition()) < tolerance;
 }
 
-bool NoteHandler::SetHeight(float height, float speed, float tolerance = 1) {
-    // elevatorPID.SetReference(height/elevatorIPR, rev::ControlType::kSmartMotion);
+// set the angle in degrees of the note handler arm
+bool NoteHandler::SetAngle(float angle, float maxRPM = 1000, float tolerance = 3) {
+    anglePID.SetSmartMotionMaxVelocity(maxRPM);
+    anglePID.SetReference(angle, rev::ControlType::kSmartMotion);
+    return abs(angle - e_angle.GetPosition()) < tolerance;
+}
 
+bool NoteHandler::SetRollerSpeed(float speed) {
+    m_rollers.Set(speed);
+    return true; // todo: return the note sensor value
+}
+
+// set the position of the rollers relative to they're current position (only use once)
+void NoteHandler::SetRollerPosition(float position) {
+    e_rollers.SetPosition(0);
+    rollersPID.SetReference(position, rev::ControlType::kPosition);
+}
+
+void NoteHandler::Initialize() {
+    m_elevator.RestoreFactoryDefaults();
+    m_elevator.SetInverted(false);
+    elevatorPID.SetP(0.2);
+    elevatorPID.SetI(0);
+    elevatorPID.SetD(1);
+    elevatorPID.SetFF(0);
+    elevatorPID.SetSmartMotionAccelStrategy(rev::SparkMaxPIDController::AccelStrategy::kTrapezoidal);
+    elevatorPID.SetSmartMotionMaxAccel(2000);
+    e_elevator.SetPositionConversionFactor(elevatorIPR);
+    m_elevator.BurnFlash();
+
+    m_angle.RestoreFactoryDefaults();
+    m_angle.SetInverted(false);
+    anglePID.SetP(0.2);
+    anglePID.SetI(0);
+    anglePID.SetD(1);
+    anglePID.SetFF(0);
+    anglePID.SetSmartMotionAccelStrategy(rev::SparkMaxPIDController::AccelStrategy::kTrapezoidal);
+    anglePID.SetSmartMotionMaxAccel(2000);
+    e_angle.SetPositionConversionFactor(angleDPR);
+    m_angle.BurnFlash();
+
+    m_rollers.RestoreFactoryDefaults();
+    m_rollers.SetInverted(false);
+    rollersPID.SetP(0.2);
+    rollersPID.SetI(0);
+    rollersPID.SetD(1);
+    rollersPID.SetFF(0);
+    e_rollers.SetPositionConversionFactor(rollerIPR);
+    m_rollers.BurnFlash();
 }
--- src/main/include/subsystems/NoteHandler.h
+++ src/main/include/subsystems/NoteHandler.h
@@ -11,24 +11,24 @@ public:
 	NoteHandler();
 
 	void Initialize();
-	bool SetHeight(float height, float speed, float tolerance = 1);
-	bool SetAngle(float angle, float speed, float tolerance = 3);
-	bool SetRoller(float speed);
-	void SetNotePosition(float position);
+	bool SetHeight(float height, float maxRPM = 1000, float tolerance = 1);
+	bool SetAngle(float angle, float maxRPM = 1000, float tolerance = 3);
+	bool SetRollerSpeed(float speed);
+	void SetRollerPosition(float position);
 
 
 private:
-	// rev::CANSparkMax m_elevator{51, rev::CANSparkMax::MotorType::kBrushless};
-	// rev::SparkPIDController elevatorPID = m_elevator.GetPIDController();
-	// rev::SparkRelativeEncoder e_elevator = m_elevator.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor);
+	rev::CANSparkMax m_elevator{51, rev::CANSparkMax::MotorType::kBrushless};
+	rev::SparkPIDController elevatorPID = m_elevator.GetPIDController();
+	rev::SparkRelativeEncoder e_elevator = m_elevator.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor);
 
-	// rev::CANSparkMax m_angle{52, rev::CANSparkMax::MotorType::kBrushless};
-	// rev::SparkPIDController anglePID = m_angle.GetPIDController();
-	// rev::SparkRelativeEncoder e_angle = m_angle.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor);
+	rev::CANSparkMax m_angle{52, rev::CANSparkMax::MotorType::kBrushless};
+	rev::SparkPIDController anglePID = m_angle.GetPIDController();
+	rev::SparkRelativeEncoder e_angle = m_angle.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor);
 
-	// rev::CANSparkMax m_rollers{53, rev::CANSparkMax::MotorType::kBrushless};
-	// rev::SparkPIDController rollersPID = m_rollers.GetPIDController();
-	// rev::SparkRelativeEncoder e_rollers = m_rollers.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor);
+	rev::CANSparkMax m_rollers{53, rev::CANSparkMax::MotorType::kBrushless};
+	rev::SparkPIDController rollersPID = m_rollers.GetPIDController();
+	rev::SparkRelativeEncoder e_rollers = m_rollers.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor);
 
 	// elevator gearbox reduction
 	const float elevatorGearboxReduction = 3;
@@ -39,4 +39,6 @@ private:
 	const float angleGearboxReduction = 3;
 	// degrees per rotation of the motor
 	const float angleDPR = 1.0/360/3/elevatorGearboxReduction;
+
+	const float rollerIPR = 1.225*M_PI;
 };
